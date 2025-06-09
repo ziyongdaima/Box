f89ab2c77ce7c3637c6b6610e960594a97054b01
blank <308407960@qq.com>
2025-06-09 14:33:49 +0800

    Update GridFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/GridFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/GridFragment.java
@@ -125,7 +125,7 @@ public class GridFragment extends BaseLazyFragment {
 
     // 获取当前页面UI的显示模式 ‘0’ 正常模式 '1' 文件夹模式 '2' 显示缩略图的文件夹模式
     public char getUITag() {
-        return (sortData == null || sortData.flag == null || sortData.flag.length() == 0) ? '0' : sortData.flag.charAt(0);
+        return (sortData.flag == null || sortData.flag.length() == 0) ? '0' : sortData.flag.charAt(0);
     }
 
     // 是否允许聚合搜索 sortData.flag的第二个字符为‘1’时允许聚搜
